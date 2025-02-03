c54ac0ae33c6c1b6f46919b8fa53124fe77a33d6
parham keyhani <parham.8484@gmail.com>
2025-02-03 17:07:41 +0330

    complete first models

--- models.h
+++ models.h
@@ -1,1 +1,22 @@
+#include "string"
+
+using namespace std;
+struct lesson
+{
+    unsigned int lesson_id;
+    string lesson_name;
+    unsigned int student_id;
+    short unsigned int vahed;
+    float grade;
+};
+
+struct person
+{
+    string name;
+    unsigned int id;
+    string reshte;
+    lesson lessons[];
+    float average;
+};
+
 
