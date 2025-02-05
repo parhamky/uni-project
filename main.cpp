e9a7e21ebe3c22f845b3be01bf723f1f150802a1
parham keyhani <parham.8484@gmail.com>
2025-02-05 03:53:41 +0330

    fixing new ui bugs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
 #include "control.h"
+#include <iostream>
 int main()
 {
     index();
+    cout << "------------------------------goodbye------------------------------" << endl;
+    return 0;
 }
